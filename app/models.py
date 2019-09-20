7c285dd0b37d4d5feb4efb0ddc44c8086dec6c11
audemed <aakash1496@gmail.com>
2019-09-20 13:45:55 +0530

    Added Lists prototype table migration script and model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,27 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    lists = db.relationship('Lists', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Lists(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    media = db.Column(db.String(7))
+    media_id = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<List {} {}>'.format(self.user_id,self.media_id)
